820f1d297f4be7272cbeddab22642e9d508b9b1f
Igor Vereshchagin <9868425@gmail.com>
2022-09-10 17:59:02 +0300

    feat: add models user and card

---
+++ models/card.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const user = require('./user');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+    match: [],
+  },
+  owner: {
+    type: mongoose.ObjectId,
+    ref: 'user',
+  },
+  likes: [{
+    type: mongoose.ObjectId,
+    default: [],
+    ref: 'user',
+    required: true,
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', userSchema);
