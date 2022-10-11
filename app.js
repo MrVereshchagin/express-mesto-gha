a5a8d200e64bd434730d52108bc8403d74370110
Igor Vereshchagin <9868425@gmail.com>
2022-10-11 21:59:10 +0300

    feat: add logger

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const cors = require('cors');
+const pageNotFound = require('./middlewares/pageNotFound');
 const { createUser, login } = require('./controllers/users');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { isAuthorized } = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { validateUser, validateAuthorization } = require('./middlewares/validation');
 
 const { PORT = 3000 } = process.env;
@@ -16,6 +18,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(requestLogger);
 
 app.use('/users', isAuthorized, userRouter);
 app.use('/cards', isAuthorized, cardRouter);
@@ -23,16 +26,15 @@ app.use('/cards', isAuthorized, cardRouter);
 app.post('/signup', validateUser, createUser);
 app.post('/signin', validateAuthorization, login);
 
-app.use('*', (req, res) => {
-  res.status(404).send({ message: 'Страница не найдена' });
-});
+app.use(pageNotFound);
+
+app.use(errorLogger);
 
 app.use(errors());
 
 // eslint-disable-next-line consistent-return
 app.use((err, req, res, next) => {
   if (err.statusCode) {
-    console.log(err.statusCode);
     return res.status(err.statusCode).send({ message: err.message });
   }
   res.status(500).send({ message: 'Что-то пошло не так' });
